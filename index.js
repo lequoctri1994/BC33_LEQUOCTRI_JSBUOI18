b72d9f960888c4551c45e9144d658d65f94c352e
lequoctri1994 <lequoctri1994@gmail.com>
2022-07-27 00:10:06 +0700

    99%

--- index.js
+++ index.js
@@ -63,29 +63,82 @@ function ex5() {
 }
 
 function ex6() {
-    var local1 = +document.getElementById("first").value;
-    var local2 = +document.getElementById("second").value;
-    var changeLocal = 0;
-    if (local1 != local2) {
-        changeLocal = exArr[local1];
-        exArr[local1] = exArr[local2];
-        exArr[local2] = changeLocal; 
+  var local1 = +document.getElementById("first").value;
+  var local2 = +document.getElementById("second").value;
+  var changeLocal = 0;
+  if (local1 != local2) {
+    changeLocal = exArr[local1];
+    exArr[local1] = exArr[local2];
+    exArr[local2] = changeLocal;
 
-        console.log(exArr)
-    }
-    document.getElementById("ex6").innerHTML = exArr;
+    console.log(exArr);
+  }
+  document.getElementById("ex6").innerHTML = exArr;
 }
 
 function ex7() {
-    for (var k = 0; k < exArr.length - 1; k++) {
-        for (var i = 0; i < exArr.length - 1; i++) {
-            if (exArr[i] > exArr[i + 1]) {
-                var temp = exArr[i];
-                exArr[i] = exArr[i + 1];
-                exArr[i + 1] = temp;
-            }
-        }
+  for (var k = 0; k < exArr.length - 1; k++) {
+    for (var i = 0; i < exArr.length - 1; i++) {
+      if (exArr[i] > exArr[i + 1]) {
+        var temp = exArr[i];
+        exArr[i] = exArr[i + 1];
+        exArr[i + 1] = temp;
+      }
     }
-       
-    document.getElementById("ex7").innerHTML = exArr;
+  }
+
+  document.getElementById("ex7").innerHTML = exArr;
+}
+
+function ex8() {
+  debugger;
+  for (var i = 0; i < exArr.length; i++) {
+    if (exArr[i] === 2 || exArr[i] === 3) {
+      break;
+    } else if(exArr[i] >= 1 &&
+      exArr[i] % 2 != 0 &&
+      exArr[i] % 3 != 0 &&
+      exArr[i] % 5 != 0) {
+      break;
+    }
+  }
+  document.getElementById("ex8").innerHTML = exArr[i];
+}
+
+var ex9Arr = [];
+function AddArrEx9() {
+  var numb = +document.getElementById("addArrayEx9").value;
+  ex9Arr.push(numb);
+
+  document.getElementById("showArrEx9").innerHTML = ex9Arr;
+}
+
+function ex9 () {
+  var numb = ex9Arr;
+  var res = "";
+  res = numb + (Number.isInteger)
+
+  document.getElementById("ex9").innerHTML = res;
+}
+
+function ex10() {
+  var num1 = 0;
+  var num2 = 0;
+  var compare = "";
+
+  for (var i = 0; i < exArr.length; i++) {
+    if (exArr[i] < 0) {
+      num1++;
+    }  else if (exArr[i] > 0) {
+      num2++;
+    };
+  };
+  if (num1 > num2) {
+    compare = `số lượng số âm > số lượng số dương`
+  } else if (num1 < num2) {
+    compare = `số lượng số âm < số lượng số dương`
+  } else if (num1 = num2) {
+    compare = `số lưỢng số âm = số lượng số dương`
+  };
+  document.getElementById("ex10").innerHTML = compare;
 }
